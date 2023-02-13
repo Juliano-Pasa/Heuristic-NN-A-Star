305e407c403a00a4f697cc24079946510a73a42b
Henrique-Liesenfeld-Krever <henriqueliesenfeld@hotmail.com>
2023-02-13 17:14:00 -0300

    feito o A* para caminhos seguros

--- 4_test.py
+++ 4_test.py
@@ -919,6 +919,63 @@ def theta(g, start, goal, v_weight, heuristic):
                 opened.remove((child, _)) # verificar
             opened.append(child, child.get_distance() + heuristic(child, goal)) # verificar'''
 
+
+def safe_astar(g, start, goal, v_weight, heuristic):
+    opened = []
+    visited = []
+
+    visibility_weight = v_weight
+
+    # Seta distância inicial para 0 e o risco inicial para o risco do ponto de partida
+    start.set_risk(start.get_local_risk())
+    start.set_distance(0)
+
+    # Calcula custo = w * risco + distancia + heursítica_r3
+    hscore = visibility_weight * start.get_risk() + start.get_distance() + heuristic(start, goal)
+
+    unvisited_queue = [(hscore, start)]
+    heapq.heapify(unvisited_queue)
+
+    count_visited = 0
+    count_open = 1
+
+    opened.append(start.get_coordinates())
+
+    while len(unvisited_queue):
+        uv = heapq.heappop(unvisited_queue)
+        current = uv[1]
+        if current == goal:
+            #print("ÇOCORRO DEUS\n\n\n\n\n",visited)
+            #break
+            distance = current.get_distance() + visibility_weight * current.get_risk()
+            path=[]
+            path=backtracking(current,start)
+            
+            #closed_nodes = list(map(lambda v: v.get_coordinates(), visited))
+            return visited, len(path), count_open, path, distance 
+
+
+        current.set_visited(True)
+        count_visited = count_visited + 1
+        visited.append(current.get_coordinates())
+
+        for next_id in current.get_neighbors():
+            next = g.get_vertex(next_id)
+            new_dist = current.get_distance() + current.get_edge_weight(next_id)
+            new_risk = current.get_risk() + next.get_local_risk()
+
+            if new_dist + visibility_weight * new_risk < next.get_distance() + visibility_weight * next.get_risk():
+                next.set_previous(current)
+                next.set_distance(new_dist)
+                next.set_risk(new_risk)
+
+                hscore = visibility_weight * new_risk + new_dist + heuristic(next, goal)
+
+                if not next.visited:
+                    heapq.heappush(unvisited_queue, (hscore, next))
+                    count_open = count_open + 1
+                    opened.append(next.get_coordinates())
+
 # A*
 def astar(g, start, goal, v_weight, heuristic):
     opened = []
